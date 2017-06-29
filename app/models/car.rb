7c1467acb2872e4c87945db31f58e995467657d6
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-06-29 17:03:02 -0400

    heroku push

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,7 +5,16 @@ class Car < ApplicationRecord
     has_many :reservations, dependent: :destroy
     has_many :reviews, dependent: :destroy
 
-    has_attached_file :avatar
+    has_attached_file :avatar,
+    :storage => :s3,
+    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+    def s3_credentials
+      {:bucket => "carbuddy",
+      :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+      :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
+      :s3_region => ENV["AWS_REGION"]}
+    end
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
     def owner
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,15 @@ class User < ApplicationRecord
     has_many :reservations
     has_many :reviews
 
-    has_attached_file :avatar
+    has_attached_file :avatar,
+    :storage => :s3,
+    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+    def s3_credentials
+      {:bucket => "carbuddy",
+      :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+      :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
+      :s3_region => ENV["AWS_REGION"]}
+    end
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
